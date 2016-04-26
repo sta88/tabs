623ed30c4ebd87ee335fd53a48ec2babf5473ee3
Tanya-S <sta-88@mail.ru>
2016-04-26 09:32:21 +0300

    Tabs

---
+++ js/script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', function(){
+
+	var tabs = document.getElementsByClassName('tabs');
+
+	tabs.addEventListener( "click" , function(event) {
+		var elem = event.target;
+		console.log(elem);
+	});
+	// $(function() {
+	// 	$('.tabs li').click(function() {
+	// 		part = $(this).find('span').attr('rel');
+	// 		$(this).addClass('active').siblings().removeClass('active');
+	// 		var imgAbout = $('#'+part);
+	// 		imgAbout.addClass('active').siblings().removeClass('active');
+	// 	}); 
+	// });
+});
