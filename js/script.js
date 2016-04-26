01db1063b5ca60773cb1bcce0ac0d14aee3abc7d
Tanya-S <sta-88@mail.ru>
2016-04-26 11:42:23 +0300

    Tabs 4

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-// "use strict"
-
-// document.addEventListener('DOMContentLoaded', function(){
-
-	// var tabs = document.getElementsByClassName('tabs');
-
-	// tabs.addEventListener( "click" , function(event) {
-	// 	var elem = event.target;
-	// 	console.log(elem);
-	// });
-	// $(function() {
-	// 	$('.tabs li').click(function() {
-	// 		part = $(this).find('span').attr('rel');
-	// 		$(this).addClass('active').siblings().removeClass('active');
-	// 		var imgAbout = $('#'+part);
-	// 		imgAbout.addClass('active').siblings().removeClass('active');
-	// 	}); 
-	// });
-// });
-
-
 (function() {
 	'use strict';
 
@@ -31,17 +10,16 @@
 				activeContent = document.querySelector('.item.active'),
 				elemAttr = elem.getAttribute('rel');
 
+			if(elem.nodeName.toLowerCase() == 'li'){
+				elem = elem.querySelector('span');
+				elemAttr = elem.getAttribute('rel');
+			}
 			activeTab.classList.remove('active');
 			activeContent.classList.remove('active');
 
 			elem.parentElement.classList.add('active');
 			document.getElementById(elemAttr).classList.add('active');
-
-
 		});
-
-
 	}; 
-
 	window.onload = foo; 
 })();
