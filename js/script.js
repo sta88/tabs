f5c1acec8743b79983990723015dc6b5c999cd73
Tanya-S <sta-88@mail.ru>
2016-05-06 17:07:51 +0300

    variables

--- js/script.js
+++ js/script.js
@@ -2,23 +2,27 @@
 	'use strict';
 
 	var foo = function() {
-		var tabs = document.getElementById('tabs');
+		var tabs = document.getElementById('tabs'),
+			tabsContent = document.getElementById('tabs-content');
+
 
 		tabs.addEventListener( "click" , function(event) {
 			var elem = event.target,
-				activeTab = document.getElementById('tabs').querySelector('.active'),
-				activeContent = document.getElementById('tabs-content').querySelector('.active'),
+				activeTab = tabs.querySelector('.active'),
+				activeContent = tabsContent.querySelector('.active'),
 				elemAttr = elem.dataset.rel;
 
 			if(elem.nodeName.toLowerCase() == 'li'){
 				return false;
 			}
+			
 			activeTab.classList.remove('active');
 			activeContent.classList.remove('active');
 
 			elem.parentElement.classList.add('active');
 			document.getElementById(elemAttr).classList.add('active');
 		});
-	}; 
+	};
+
 	window.onload = foo; 
 })();
