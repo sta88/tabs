339719481fa7abc8747eaec356a734c0e276689b
Tanya-S <sta-88@mail.ru>
2016-05-06 16:17:26 +0300

    data & id

--- js/script.js
+++ js/script.js
@@ -2,17 +2,18 @@
 	'use strict';
 
 	var foo = function() {
-		var tabs = document.getElementsByClassName('tabs')[0];
+		var tabs = document.getElementById('tabs');
 
 		tabs.addEventListener( "click" , function(event) {
 			var elem = event.target,
 				activeTab = document.querySelector('.tabs .active'),
 				activeContent = document.querySelector('.item.active'),
-				elemAttr = elem.getAttribute('rel');
+				elemAttr = elem.dataset.rel;
 
 			if(elem.nodeName.toLowerCase() == 'li'){
-				elem = elem.querySelector('span');
-				elemAttr = elem.getAttribute('rel');
+				// elem = elem.querySelector('span');
+				// elemAttr = elem.dataset.rel;
+				return false;
 			}
 			activeTab.classList.remove('active');
 			activeContent.classList.remove('active');
